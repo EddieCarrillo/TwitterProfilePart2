409f0f95afda7a7aff0331cb17651f07009e65e6
Eduardo Carrillo <edcarril@ucsd.edu>
2018-04-05 21:41:38 -0700

    Initial Commit

---
+++ NavbarImageStickyTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavbarImageStickyTutorial
+//
+//  Created by Eduardo Carrillo on 4/5/18.
+//  Copyright © 2018 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
